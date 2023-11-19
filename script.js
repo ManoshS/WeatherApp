3add2995da4335e13b837543271defb8bfbfebdd
Manosh <smanosh73@gmail.com>
2023-11-19 13:15:51 +0530

    adding proper temp

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function checkWeather(city) {
     } else {
         var data = await response.json();
         document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp - 273.15) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
